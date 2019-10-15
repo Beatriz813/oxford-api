6275b974f3aea5b3080f94775dc526a160d4773d
Beatriz813 <beatrizalcantara813@gmail.com>
2019-10-15 11:52:58 -0300

    Preparando a reposta de pronunciations

--- oxford/server.js
+++ oxford/server.js
@@ -4,8 +4,6 @@ const cors = require('cors')
 
 const app_id = '1164be52' // insert your APP Id
 const app_key = '2098d3171e9755cde1e03c6a4a30d0b5' // insert your APP Key
-let wordId = ''
-const fields = 'pronunciations'
 const strictMatch = 'false'
 let body = ''
 // Header para a requisição Oxford
@@ -25,27 +23,35 @@ app.get('/:campo/:palavra', (req, res) => {
   }
   // Fazendo a requisição para a API Oxford
   https.get(options, (resp) => {
-      resp.on('data', (d) => {
-          body += d
-      })
-      resp.on('end', () => {
-          body = JSON.parse(body)
-          let resposta = []
-          let entradasLexicais = []
-          entradasLexicais = body.results.filter(elemento => elemento.lexicalEntries.length > 0)  // Só vai retornar os objetos com a propriedade lexicalEntries
-          entradasLexicais.forEach(resultados => {
-            resultados.lexicalEntries.forEach(entradas => {
-              resposta = resposta.concat(entradas.entries)
-            })
+    resp.on('data', (d) => {
+      body += d
+    })
+    resp.on('end', () => {
+      body = JSON.parse(body)
+      let resposta = []
+      let entradasLexicais = []
+      if (req.params.campo === 'pronunciations') {
+        // res.end(JSON.stringify(body))
+        entradasLexicais = body.results.find((resultados) => {
+          return resultados.lexicalEntries.find(elemento => elemento.pronunciations)
+        })
+        res.end(JSON.stringify(entradasLexicais))
+      } else {
+        entradasLexicais = body.results.filter(elemento => elemento.lexicalEntries.length > 0) // Só vai retornar os objetos com a propriedade lexicalEntries
+        entradasLexicais.forEach(resultados => {
+          resultados.lexicalEntries.forEach(entradas => {
+            resposta = resposta.concat(entradas.entries)
           })
-          res.end(JSON.stringify(resposta))
-          body = ''
-      })
-      resp.on('error', (e) => {
-        console.log(e)
-        res.end('Deu ruim')
-      })
+        })
+      }
+      res.end(JSON.stringify(resposta))
+      body = ''
+    })
   })
+  res.on('error', (e) => {
+    console.log(e)
+    res.end('Deu ruim')
+  }) // on error
 })
 
 app.listen(3000)
